9a4f1db79c83a8a2f2e6d46dcee273caed1ff89e
Alexis CHEVALLIER <alexischevallier93+git@gmail.com>
2023-01-13 19:08:42 +0100

    fix: correction du bootstrap packer

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,6 +1,6 @@
 local ensure_packer = function()
   local fn = vim.fn
-  local install_path = fn.stdpath('data')..'%LOCALAPPDATA%\\nvim-data\\site\\pack\\packer\\start\\packer.nvim'
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
   if fn.empty(fn.glob(install_path)) > 0 then
     fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
     vim.cmd [[packadd packer.nvim]]
@@ -8,6 +8,8 @@ local ensure_packer = function()
   end
   return false
 end
+
+local packer_bootstrap = ensure_packer()
 
 require('packer').startup({function(use)
   use 'wbthomason/packer.nvim' -- Packer can manage itself
@@ -104,6 +106,11 @@ require('packer').startup({function(use)
         -- Buffer delete
         use 'famiu/bufdelete.nvim'
 
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end,
 config = {
     display = {
