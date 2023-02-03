817f657368956c84e52af6624413d01b5f44066a
Alexis CHEVALLIER <alexischevallier93+git@gmail.com>
2023-02-03 17:44:41 +0100

    changed require order + comments

--- init.lua
+++ init.lua
@@ -8,15 +8,22 @@ vim.api.nvim_exec('language en_US.utf8', true)
 -- Required plugins and configs --
 ----------------------------------
 
+-- user scripts
+
+require('scripts.reload')
+
+require('scripts.nvim_tree_open')
+
+-- user plugins 
+
 require('user.plugins')
 
 require('user.plugins_config')
 
+require('user.coc_config')
+
+-- user config
+ 
 require('user.config')
 
 require('user.mappings')
-
-require('scripts.reload')
-
-require('user.coc_config')
-
