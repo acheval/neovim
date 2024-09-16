6b50185a698a9fa2fd698502491f7e4c83baee4e
alexis <alexis@debian-BULLSEYE-live-builder-AMD64>
2024-09-16 18:55:53 +0200

    removed obsolete config from gitsigns

--- lua/user/plugins_config.lua
+++ lua/user/plugins_config.lua
@@ -564,14 +564,15 @@ require('scrollbar').setup({
     },
 })
 
+
 require('gitsigns').setup({
     signs = {
-        add          = { hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'    },
-        change       = { hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn' },
-        delete       = { hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn' },
-        topdelete    = { hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn' },
-        changedelete = { hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn' },
-        untracked    = { hl = 'GitSignsAdd'   , text = '┆', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'    },
+        add          = { text = '│' },
+        change       = { text = '│' },
+        delete       = { text = '_' },
+        topdelete    = { text = '‾' },
+        changedelete = { text = '~' },
+        untracked    = { text = '┆' },
     },
     signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
     numhl      = true, -- Toggle with `:Gitsigns toggle_numhl`
@@ -601,10 +602,7 @@ require('gitsigns').setup({
           relative = 'cursor',
           row = 0,
           col = 1
-    },
-    yadm = {
-      enable = false
-    },
+    }
 })
 
 require('ibl').setup({
