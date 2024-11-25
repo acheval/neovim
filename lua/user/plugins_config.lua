3da10f43005287e494f7205dc1a238b82d42901d
Alexis CHEVALLIER <alexis-externe.chevallier@edf.fr>
2024-11-25 14:19:01 +0100

    chore: modified ignore true -> false

--- lua/user/plugins_config.lua
+++ lua/user/plugins_config.lua
@@ -325,7 +325,7 @@ require('nvim-tree').setup({
     },
     git = {
         enable = true,
-        ignore = true,
+        ignore = false,
         show_on_dirs = true,
         show_on_open_dirs = true,
         timeout = 400
