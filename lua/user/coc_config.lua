8eba1715192fcf772e68211b9d88fa3d40fe7e89
Alexis CHEVALLIER <alexis-externe.chevallier@edf.fr>
2024-12-04 10:43:39 +0100

    moved coc_config

--- lua/user/coc_config.lua
+++ lua/user/coc_config.lua
@@ -15,7 +15,6 @@ local keyset = vim.keymap.set
 function _G.check_back_space()
     local col = vim.fn.col('.') - 1
     return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
-end
 
 -- Use Tab for trigger completion with characters ahead and navigate
 -- NOTE: There's always a completion item selected by default, you may want to enable
