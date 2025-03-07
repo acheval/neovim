8197e4b6eac4920e5e8bfd4a8dea75a18ac48084
Alexis CHEVALLIER <alexis-externe.chevallier@edf.fr>
2025-03-07 10:18:31 +0100

    chore: cleaned up init.lua

--- init.lua
+++ init.lua
@@ -8,24 +8,16 @@ vim.api.nvim_exec('language en_US.utf8', true)
 -- Required plugins and configs --
 ----------------------------------
 
--- user scripts
-
-require('scripts.reload')
-
--- user plugins 
-
-require('user.plugins')
-
-require('user.plugins_config')
-
--- coc configs
-
-require('user.coc.coc_config')
-
-require('user.coc.coc_extensions')
-
 -- user config
  
 require('user.config')
 
 require('user.mappings')
+
+-- user scripts
+
+require('functions.reload')
+
+-- base config
+
+require('config.lazy')
